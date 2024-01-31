43ac5fe6de940f20b3a32f52c41547bab6a253ca
Gabriel Santos <biel2013as@gmail.com>
2024-01-31 19:29:07 -0300

    Saving data in local storage

---
+++ script.js
@@ -0,0 +1,56 @@
+function register() {
+    const userName = document.getElementById("username").value;
+    const mail = document.getElementById("mail").value;
+    const password = document.getElementById("password").value;
+    const cPassword = document.getElementById("cPassword").value;
+
+    if (password === cPassword) {
+        let newUser = {
+            name: userName,
+            email: mail,
+            password: password
+        };
+
+        let existingData = localStorage.getItem("userData");
+
+        if (existingData) {
+            try {
+                existingData = JSON.parse(existingData);
+                
+                if (!Array.isArray(existingData)) {
+                    existingData = [];
+                }
+            } catch (error) {
+                existingData = [];
+            }
+            
+            existingData.push(newUser);
+        } else {
+            existingData = [newUser];
+        }
+
+        localStorage.setItem("userData", JSON.stringify(existingData));
+    } else {
+        alert("Senhas diferentes");
+    }
+}
+
+function login(){
+    const email = document.getElementById("loginEmail").value
+    const password = document.getElementById("loginPassword").value
+
+    const savedData = localStorage.getItem("userData")
+
+    if (savedData) {
+        const users = JSON.parse(savedData);
+
+        const user = users.find(user => user.email === email);
+
+        if (user && user.password === password){
+            alert("login bem sucedido")
+        }
+        else {
+            alert("nenhum usuario encontrado")
+        }
+    }
+}
